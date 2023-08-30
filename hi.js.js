0b5ff7e73158ac033a9835f6acff49bd8ad5edef
Victor68888 <116842700+Victor68888@users.noreply.github.com>
2023-08-30 08:45:18 +0800

    Add files via upload

---
+++ hi.js.js
@@ -0,0 +1,32 @@
+// 1
+const myBoolean = true;
+
+// 2
+const myString = 'hello world';
+
+// 3
+const firstNumber = 20;
+
+// 4 - notice how I have used `let` here.  We have to use this because we re-assign the value of this variable
+let secondNumber = 40;
+
+// 5
+secondNumber = 80;
+
+// 6
+const myArray = [myBoolean, myString];
+
+//7
+const myObject = {
+    firstProperty: myArray,
+    sumProperty: firstNumber + secondNumber
+};
+
+//8
+console.log(myObject);
+//9
+console.log(myObject.sumProperty);
+
+//10
+console.log(myObject.firstProperty[1]);
+
